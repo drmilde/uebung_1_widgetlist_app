59d44649cfd394fcdffcb438bc415b2daf3d1e87
KilianFH <kilianeller12@outlook.com>
2019-10-23 14:01:59 +0200

    Serag - Opacity

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class _WidgetListeStudierendeScreenState
                   zeileMitWidgetUndStudierendem("Widgetname", "Studierender"),
                   zeileMitWidgetUndStudierendem("Image", "Andreas Enns"),
                   zeileMitWidgetUndStudierendem("Spacer", "Dan Vi Nguyen"),
+                  zeileMitWidgetUndStudierendem("Opacity", "Serageldin El-Khodary"),
                   //
                   // TODO Hier Eure jeweilige Zeile einfügen !!!!!!
                   // Danach dann testen, ob es läuft und
