60c7c81f8163899814a4589034e4246986aa49f3
Maximilian Riedl <maximilian.riedl@informatik.hs-fulda-de>
2019-10-23 12:20:32 +0200

    Neuer Try

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,7 @@ class _WidgetListeStudierendeScreenState
               Column(
                 children: <Widget>[
                   zeileMitWidgetUndStudierendem("Widgetname", "Studierender"),
+                  zeileMitWidgetUndStudierendem("Image", "Andreas Enns"),
                   zeileMitWidgetUndStudierendem("RichText", "Maximilian Riedl"),
                   //
                   // TODO Hier Eure jeweilige Zeile einfügen !!!!!!
