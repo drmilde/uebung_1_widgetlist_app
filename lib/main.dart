cf741d68718e293766a344e92f9e49ea8c53fa64
tonihoyer <antonia.hoyer@informatik.hs-fulda.de>
2019-10-29 14:05:01 +0100

    raised button

--- lib/main.dart
+++ lib/main.dart
@@ -60,6 +60,7 @@ class _WidgetListeStudierendeScreenState
                   zeileMitWidgetUndStudierendem("Table", "Patrick Finger"),
                   zeileMitWidgetUndStudierendem("Safe Area", "Kathleen Wrede"),
                   zeileMitWidgetUndStudierendem("SizedBox", "Jana Bleidorn"),
+                  zeileMitWidgetUndStudierendem("Raised Button", "Antonia Hoyer"),
 
                   //
                   // TODO Hier Eure jeweilige Zeile einfügen !!!!!!
